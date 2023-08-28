1d0f5012ee96f7323c45ded2d5169fd1699b4f81
Samuel Arantes <65547548+Samuel-pixe@users.noreply.github.com>
2023-08-28 14:13:44 -0300

    Add files via upload

---
+++ exemplo01/HelloWorld.java
@@ -0,0 +1,14 @@
+package exemplo01;
+
+import javax.swing.JOptionPane;
+
+public class HelloWorld {
+    
+    public static void main(String[] args){
+	
+		JOptionPane.showMessageDialog(
+			null,
+			"Hello World em Java !!!"
+		);
+    }
+}
